8ca8f51a6fc6c9a70d7b84f40969d6ee358c4e43
Johan Traconiz <johan.traconiz@gmail.com>
2026-09-20 16:31:33 -0600

    Rename directories to lowercase

--- NodeJS_TEMP/01-helloWord.js
+++
@@ -1,6 +0,0 @@
-console.log('Hola, Mundo.');
-
-console.log('\nMI PRIMERA SUMA.');
-let a = 5;
-let b = 10;
-console.log(`El resultado de la suma es: ${a+b}`);
--- NodeJS_TEMP/fetch.js
+++
@@ -1,5 +0,0 @@
-console.log('Hola, Johan');
-
-fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-.then(res => res.json())
-.then(data => console.log(data));
